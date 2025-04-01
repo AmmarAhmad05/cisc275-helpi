a3ebf1d53e6fc5bfa2e28a98e6994946db4c0d20
Olubakinde Oluwatito <tolubaki@udel.edu>
2025-04-01 19:51:41 -0400

    Header added to the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const About: React.FC = () => {
   return (
     <div className="page">
+      <Header />
       <h2>About Career Helpi</h2>
       <p>Learn more about our mission and how we guide users in their career journey.</p>
     </div>
